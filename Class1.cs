c8edbdaafde949d7b5a091ed67aee17cc8cbbaca
adamberglund <rca.berglund@gmail.com>
2020-02-23 19:58:13 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A2
+{
+    class Menu
+    {
+		public void Start()
+		{
+			int choice = -1;
+
+			while (choice != 0)
+			{
+				WriteMenuText(); // show the menu
+				Console.Clear();
+
+			}
+
+
+
+		}
+
+		private void WriteMenuText()
+		{
+
+			Console.Clear();
+			Console.Write("----------------------------------------------------------------------");
+			Console.Write("                           MAIN MENU                                  ");
+			Console.Write("----------------------------------------------------------------------");
+			Console.Write("             Whole numbers with FOR                              : 1  ");
+			Console.Write("             Floating poin numbers with WHILE                    : 2  ");
+			Console.Write("             Currency converter with DO WHILE LOOP               : 3  ");
+			Console.Write("             Work shedule                                        : 4  ");
+			Console.Write("             Temperature table                                   : 5  ");
+			Console.Write("                                                                      ");
+			Console.Write("             Exit the program                                    : 0  ");
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A2
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Start();
+        }
+    }
+}
