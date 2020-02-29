d31f2040d1d69cce8968f89bbfbbf12519e1962f
adamberglund <rca.berglund@gmail.com>
2020-02-29 21:01:48 +0100

    lite nya funktioner

---
+++ Menu.cs
@@ -0,0 +1,94 @@
+﻿/*
+Denna fil har skapats som en del av kursen [C# I] på Malmö Universitet 2020
+Namn: Carl-Adam Berglund
+e-mail: ak7764@mau.se
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace A2
+{
+    class Menu
+    {
+		public void Start()
+		{
+			int choice = -1;
+
+			while (choice != 0)
+			{
+				WriteMenuText(); // show the menu
+								 // Read user choice
+				choice = int.Parse(Console.ReadLine());
+
+
+				// Depending on the value fo the choice, create an
+				// instance of the class displayed on the menu
+
+				switch (choice)
+				{
+					case 1: // menu item 1 (The for-statement)
+						{
+							WholeNumbersForAdd sumObj = new WholeNumbersForAdd();
+							sumObj.Start();
+							break;
+						}
+					case 2: //menu item 2 
+						{
+							Console.Write(" ** You have pressed 2 ** NOT implemented yet");
+							Console.ReadLine();
+							break;
+						}
+					case 3: //menu item 3
+						{
+							Console.Write(" ** You have pressed 3 ** NOT implemented yet");
+							break;
+						}
+					case 4: //menu item 4
+						{
+							Console.Write(" ** You have pressed 4 ** NOT implemented yet");
+							break;
+						}
+					case 5: //menu item 5
+						{
+							Console.Write(" ** You have pressed 5 ** NOT implemented yet");
+							break;
+						}
+					case 0: //menu item 0
+						{
+							Console.Write(" ** You have pressed 0 ** NOT implemented yet");
+							break;
+						}
+					default:
+						{
+							Console.WriteLine($"Invalid Input, pleade try againg ({choice})");
+							Console.ReadKey();
+							break;
+						}
+				}
+
+			}
+
+
+
+		}
+
+		private void WriteMenuText()
+		{
+
+			Console.Clear();
+			Console.Write("\n----------------------------------------------------------------------");
+			Console.Write("\n                           MAIN MENU                                  ");
+			Console.Write("\n----------------------------------------------------------------------");
+			Console.Write("\n        Whole numbers with FOR                                     : 1");
+			Console.Write("\n        Floating poin numbers with WHILE                           : 2");
+			Console.Write("\n        Currency converter with DO WHILE LOOP                      : 3");
+			Console.Write("\n        Work shedule                                               : 4");
+			Console.Write("\n        Temperature table                                          : 5");
+			Console.Write("\n                                                                      ");
+			Console.Write("\n        Exit the program                                           : 0");
+			Console.WriteLine();
+
+		}
+	}
+}
---
+++ WholeNumbersForAdd.cs
@@ -0,0 +1,98 @@
+﻿/*
+Denna fil har skapats som en del av kursen [C# I] på Malmö Universitet 2020
+Namn: Carl-Adam Berglund
+e-mail: ak7764@mau.se
+*/
+
+
+using System;
+
+namespace A2
+{
+    class WholeNumbersForAdd
+    {
+
+        // int number;
+        //public void-method that preforms the whole process
+        public void Start()
+        {
+            // Call the method which writes the program info, title, etc.
+            WriteProgramInfo();
+            ReadInput(WriteProgramInfo());
+            SumNumber();
+            ShowResults();
+            Console.ReadLine();
+        }
+
+        private void ShowResults()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Nästa metod att inplementera
+        /// </summary>
+        /// <returns>Sum of numbers chosen</returns>
+
+        private long SumNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// ReadInput reads a key from user and evaluate it. If its a number return it else make
+        /// user try again
+        /// then summs the numbers and returns them.
+        /// </summary>
+        private bool keyOk; // manage ReadInput while loop
+        private int userInput;
+
+        private int ReadInput(int sizeOf)
+        {
+            int x = 0;
+            int[] theNumbers = new int[sizeOf];
+            for (int i = WriteProgramInfo(); i < WriteProgramInfo(); i++)
+            {
+
+                theNumbers[i] = Input();
+            }
+            for (int i = WriteProgramInfo(); i < WriteProgramInfo(); i++)
+            {
+                x += theNumbers[i];
+            }
+
+
+            return x;
+        }
+
+        public int WriteProgramInfo()
+        {
+            Console.WriteLine();
+            Console.Write("\n***** Summation of whole numbers ******");
+            Console.Write("\n       using a for statement \n\n----------------------------------------");
+            Console.Write("\nHow many numbers would you like to add? ");
+            int numberOfNumbersToAdd = int.Parse(Console.ReadLine());
+            return numberOfNumbersToAdd;
+
+        }
+        private int Input()
+        {
+            while (keyOk == false)
+            {
+                try
+                {
+                    userInput = int.Parse(Console.ReadLine());
+                    return userInput;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not a number!");
+                    keyOk = false;
+                    return -1;
+                }
+            }
+            return -1;
+        }
+
+    } // end class
+} // end namespace 
